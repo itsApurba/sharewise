f7dbc6e816ecaaf218fe39d013dd2aa8eaf556de
Apurba Adhikary <apurba.apurbaadhikary@gmail.com>
2023-05-23 19:28:21 +0000

    Added: New API

--- frontend/api/postdata.js
+++ frontend/api/postdata.js
@@ -1,11 +1,31 @@
-import axios from 'axios'
+import axios from "axios";
+
+export const getAllPosts = async () => {
+  const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/posts`);
+  return res.data;
+};
+
+export const getPostById = async (id) => {
+  const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/posts/${id}`);
+  return res.data;
+};
 
 export const createPost = async (postData) => {
-    const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/posts`, postData)
-    return res.data
-}
+  const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/posts`, postData);
+  return res.data;
+};
 
 export const updatePostByID = async (postData) => {
-    const res = await axios.put(`${import.meta.env.VITE_BACKEND_URL}/api/posts/${postData.id}`, postData)
-    return res.data
-}
+  const res = await axios.put(`${import.meta.env.VITE_BACKEND_URL}/api/posts/${postData.id}`, postData);
+  return res.data;
+};
+
+export const likePost = async (id) => {
+  const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/posts/${id}/like`);
+  return res.data;
+};
+
+export const dislikePost = async (id) => {
+  const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/posts/${id}/unlike`);
+  return res.data;
+};
--- frontend/api/userdata.js
+++ frontend/api/userdata.js
@@ -9,3 +9,8 @@ export const updateUserByID = async (user) => {
   const res = await axios.put(`${import.meta.env.VITE_BACKEND_URL}/api/users/${user.id}`, user);
   return res.data;
 }
+
+export const getUserByID = async (id) => {
+  const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/users/${id}`);
+  return res.data;
+}
