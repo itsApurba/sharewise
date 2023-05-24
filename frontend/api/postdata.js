81d2de2b3d0af2e22a8802d29453136b25d4e91a
Apurba Adhikary <apurba.apurbaadhikary@gmail.com>
2023-05-24 10:05:23 +0000

    API Fixes

--- frontend/api/postdata.js
+++ frontend/api/postdata.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getAllPosts = async () => {
   const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/posts`);
-  return res.data;
+  return res.data.reverse();
 };
 
 export const getPostById = async (id) => {
@@ -11,12 +11,19 @@ export const getPostById = async (id) => {
 };
 
 export const createPost = async (postData) => {
+  console.log(postData);
   const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/posts`, postData);
   return res.data;
 };
 
-export const updatePostByID = async (postData) => {
-  const res = await axios.put(`${import.meta.env.VITE_BACKEND_URL}/api/posts/${postData.id}`, postData);
+export const updatePostByID = async ({ id, content }) => {
+  console.log(id, content);
+  const res = await axios.put(`${import.meta.env.VITE_BACKEND_URL}/api/posts/${id}`, { content });
+  return res.data;
+};
+
+export const deletePostByID = async (id) => {
+  const res = await axios.delete(`${import.meta.env.VITE_BACKEND_URL}/api/posts/${id}`);
   return res.data;
 };
 
